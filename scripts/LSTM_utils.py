f9a612e70a3b9fa11e2185afaf9338b27c51e8e6
AxelFritz1 <fritz.axel@outlook.com>
2024-01-28 19:12:45 +0100

    add LSTM volatility

--- scripts/LSTM_utils.py
+++ scripts/LSTM_utils.py
@@ -6,3 +6,10 @@ def prepare_data(data, n_steps):
         x.append(data[i:(i + n_steps), 0])
         y.append(data[i + n_steps, 0])
     return np.array(x), np.array(y)
+
+def prepare_data2(data, n_steps, n_future=7):
+    x, y = [], []
+    for i in range(len(data) - n_steps - n_future + 1):
+        x.append(data[i:(i + n_steps), 0])
+        y.append(data[(i + n_steps):(i + n_steps + n_future), 0])
+    return np.array(x), np.array(y)
