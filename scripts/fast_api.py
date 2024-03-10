40500ee4661d0429eb6ad9b51892f73edb2922b5
AxelFritz1 <fritz.axel@outlook.com>
2024-03-10 17:45:54 +0100

    fix fastapi

--- scripts/fast_api.py
+++ scripts/fast_api.py
@@ -5,13 +5,26 @@ from pydantic import BaseModel
 from main import *
 
 class User_input(BaseModel):
-    tickers:list
-    model:str
-    target :str
+    tickers : list
+    model : str
+    target : str
     horizon : int
 
 app = FastAPI(debug = True)
 @app.post("/prediction")
-def operate(input: User_input):
-    pred, ptf, error = forecast(User_input.tickers,User_input.model,User_input.target,User_input.horizon).run()
-    return pred, ptf, error
+def operate(input:User_input):
+    model = forecast(input.tickers,
+                     input.model,
+                     input.target,
+                     input.horizon)
+
+    pred, ptf, error = model.run()
+
+    pred_json = {key: value.to_json(orient="records") for key, value in pred.items()}
+
+    ptf_json = ptf.to_json(orient="records")
+    error_json = error.to_json(orient="records")
+
+    return {"predictions": pred_json,
+            "portfolio": ptf_json,
+            "error": error_json}
